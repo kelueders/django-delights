f5158e489909d321f2fd07e72677b9ca47f12b75
kelueders <kelueders@gmail.com>
2024-11-05 21:36:43 -0600

    add inventory ingredient list view

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "inventory"
 urlpatterns = [
+    path("", views.home, name="home"),
     path("ingredient/list", views.IngredientList.as_view(), name="ingredientlist")
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -4,5 +4,8 @@ from django.views.generic import ListView
 from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 
 # Create your views here.
+def home(request):
+    return render(request, "inventory/home.html")
+
 class IngredientList(ListView):
     model = Ingredient
